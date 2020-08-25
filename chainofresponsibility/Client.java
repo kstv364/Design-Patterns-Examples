0e4de294189c10977dc7c974c7711a2dca0da183
Kaustav Chanda <kstvkmrchanda2@gmail.com>
2020-08-25 21:33:52 +0530

    Chain of Responsibility added

---
+++ chainofresponsibility/Client.java
@@ -0,0 +1,27 @@
+package practice.chainofresponsibility;
+
+import java.time.LocalDate;
+
+import practice.chainofresponsibility.LeaveApplication.Type;
+
+public class Client {
+
+	public static void main(String[] args) {
+	   LeaveApplication application = LeaveApplication.getBuilder().withType(Type.Sick)
+			   	                      .from(LocalDate.now()).to(LocalDate.of(2018, 2, 28))
+			   	                      .build();
+	   System.out.println(application);
+	   System.out.println("**************************************************");
+	   LeaveApprover approver = createChain();
+	   approver.processLeaveApplication(application);
+	   System.out.println(application);
+	}
+
+	private static LeaveApprover createChain() {
+		Director director = new Director(null);
+		Manager manager = new Manager(director);
+		ProjectLead lead = new ProjectLead(manager);
+		return lead;
+	}
+	
+}
---
+++ chainofresponsibility/LeaveApprover.java
@@ -0,0 +1,11 @@
+package practice.chainofresponsibility;
+
+
+
+//This represents a handler in chain of responsibility
+public interface LeaveApprover {
+
+	void processLeaveApplication(LeaveApplication application);
+	
+	String getApproverRole();
+}
